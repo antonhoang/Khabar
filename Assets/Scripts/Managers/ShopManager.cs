0050d75c23ea632bb41ee88818ee3e82ab7d9f12
antonhoang <macbook_alina@macbook-air-alina.home>
2024-01-24 12:21:13 +0100

    add shopmanager impl

--- Assets/Scripts/Managers/ShopManager.cs
+++ Assets/Scripts/Managers/ShopManager.cs
@@ -2,20 +2,59 @@
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine.EventSystems;
 
 public class ShopManager : MonoBehaviour
 {
     GameObject itemTemplate;
     GameObject g;
     [SerializeField] Transform scrollview;
+    public List<ShopItem> ShopItemsList;
+    public GameObject KhabarDetailPopup;
+
+    [System.Serializable]
+    public class ShopItem
+    {
+        public Sprite Image;
+        public int Price;
+        public bool IsPurchased = false;
+    }
+
+    Button buyBtn;
 
     void Start()
     {
+        int len = ShopItemsList.Count;
         itemTemplate = scrollview.GetChild(0).gameObject;
-        for (int i =0; i < 20; i++)
+        for (int i =0; i < len; i++)
         {
             g = Instantiate(itemTemplate, scrollview);
+
+            Image imageComponent = g.transform.GetChild(0).GetComponent<Image>();
+            
+            imageComponent.sprite = ShopItemsList[i].Image;
+            Color currentColor = imageComponent.color;
+            imageComponent.color = new Color(currentColor.r, currentColor.g, currentColor.b, 0.5f);
+            g.transform.GetChild(1).GetComponent<TMP_Text>().text = ShopItemsList[i].Price.ToString();
+            buyBtn = g.transform.GetChild(2).GetComponent<Button>();
+            g.transform.GetChild(3).GetComponent<Image>().gameObject.SetActive(true);
+
+
+            EventTrigger trigger = g.AddComponent<EventTrigger>();
+
+            // Add a pointer click event to the EventTrigger
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.PointerClick;
+            entry.callback.AddListener((data) => { OnPanelClick(g); });
+            trigger.triggers.Add(entry);
         }
         Destroy(itemTemplate);
+    }
+
+    void OnPanelClick(GameObject panel)
+    {
+        Debug.Log("Panel Clicked!");
+        KhabarDetailPopup.SetActive(true);
     }
 }
